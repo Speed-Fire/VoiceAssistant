3acf86bfa8522e7124a15722816769e1e684e57e
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-05-27 21:31:14 +0200

    Adding project with common classes.

---
+++ VoiceAssistant.Core/Misc/OneOf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+
+namespace VoiceAssistant.Core.Misc
+{
+	public class OneOf<T1, T2>
+	{
+		public T1 First { get; }
+		public T2 Second { get; }
+
+		public bool IsFirst => First != null;
+
+        public OneOf(T1 value)
+        {
+            First = value;
+        }
+
+		public OneOf(T2 value)
+		{
+			Second = value;
+		}
+	}
+}
---
+++ VoiceAssistant.Core/Misc/Provider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Core.Misc
+{
+    public class Provider<T> : INotifyPropertyChanged
+        where T : class
+    {
+        private T? _value;
+        public T? Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+
+                if(_value is not null && _value is IDisposable disposable)
+                    disposable.Dispose();
+
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
