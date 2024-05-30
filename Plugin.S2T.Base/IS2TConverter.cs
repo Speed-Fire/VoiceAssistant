132ddcab07f12fa893b8448f6212e6969da9ddc4
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-05-30 23:14:02 +0200

    Adding speech to text plugin base.

---
+++ Plugin.S2T.Base/IS2TConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoiceAssistant.Core.Misc;
+
+namespace Plugin.S2T.Base
+{
+	/// <summary>
+	/// Speech to text converter interface.
+	/// </summary>
+	public interface IS2TConverter : IDisposable
+	{
+		/// <summary>
+		/// Converts speech to text.
+		/// </summary>
+		/// <param name="input">Stream with an audio.</param>
+		/// <param name="timeot">Timeout.</param>
+		/// <returns>Recognized text or error.</returns>
+		Task<OneOf<string, Exception>> Convert(Stream input, int timeot = 3000);
+	}
+}
---
+++ Plugin.S2T.Base/S2TConverterInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.S2T.Base
+{
+	public class S2TConverterInfo(string name, string description, Func<IS2TConverter> converterFactory)
+	{
+		public string Name { get; } = name;
+		public string Description { get; } = description;
+		public Func<IS2TConverter> ConverterFactory { get; } = converterFactory;
+	}
+}
