8f35bc93e3643b189aa0e27e786c7c2c85045a25
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-05-30 23:13:31 +0200

    Adding settings loading service (intended for plugins).

---
+++ VoiceAssistant.Core/Interfaces/ISettingsLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Core.Interfaces
+{
+	public interface ISettingsLoader
+	{
+		void Load();
+	}
+}
---
+++ VoiceAssistant.Services/Extensions/DIExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Services.Extensions
+{
+	public static class DIExtensions
+	{
+		public static IServiceCollection RegisterServices(this IServiceCollection services)
+		{
+			services
+				.AddTransient<SettingsLoadingService>();
+
+			return services;
+		}
+	}
+}
