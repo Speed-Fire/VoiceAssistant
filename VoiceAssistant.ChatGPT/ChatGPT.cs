8eb10280ff081745d27fbaa46e7d10874fffa824
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-06-05 02:46:35 +0200

    Adding Python to C# ChatGPT adapter.

---
+++ VoiceAssistant.ChatGPT/ChatGPT.cs
@@ -0,0 +1,40 @@
+﻿using CSPythonInvoker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.ChatGPT
+{
+	public class ChatGPT
+	{
+		private const string MEMBER_SYSTEM_COMMAND = "systemCommand";
+		private const string MEMBER_MODEL = "model";
+		private const string METHOD_SEND_MESSAGE = "sendMessage";
+
+		private readonly PInstance _instance;
+
+		internal ChatGPT(PInstance instance)
+		{
+			_instance = instance;
+		}
+
+		public string SystemCommand
+		{
+			get => _instance.GetMember<string>(MEMBER_SYSTEM_COMMAND);
+			set => _instance.SetMember(MEMBER_SYSTEM_COMMAND, value);
+		}
+
+		public string Model
+		{
+			get => _instance.GetMember<string>(MEMBER_MODEL);
+			set => _instance.SetMember(MEMBER_MODEL, value);
+		}
+
+		public string SendMessage(string message)
+		{
+			return _instance.CallFunction(METHOD_SEND_MESSAGE, message);
+		}
+	}
+}
---
+++ VoiceAssistant.ChatGPT/Extensions/DIExtensions.cs
@@ -0,0 +1,57 @@
+﻿using CSPythonInvoker;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.ChatGPT.Extensions
+{
+	public static class DIExtensions
+	{
+		private const string KEY = "VoiceAssistant.ChatGPT";
+
+		public static IServiceCollection RegisterChatGPT(this IServiceCollection services)
+		{
+			services.AddKeyedSingleton(KEY, (provider, key) =>
+			{
+				// getting main script
+				using var stream = Assembly.GetExecutingAssembly()
+					.GetManifestResourceStream("VoiceAssistant.ChatGPT.Resources.ChatGPT.py");
+				if (stream is null)
+					throw new InvalidOperationException("Main script is not found!");
+				using var reader = new StreamReader(stream);
+
+				var code = reader.ReadToEnd();
+
+				// getting python environment
+				var env = provider.GetRequiredService<PEnvironment>();
+
+				// creating scope with main script dependencies
+				var scope = env.CreateScopeWithDependencies(
+					Assembly.GetExecutingAssembly(),
+					"VoiceAssistant.ChatGPT.Resources.scripts.zip");
+
+				// loading main script into scope.
+				scope.LoadScriptFromString(code);
+
+				return scope;
+			});
+
+			services.AddTransient(provider =>
+			{
+				var scope = provider.GetRequiredKeyedService<PScope>(KEY);
+
+				var instance = scope.CreateInstance("ChatGPT");				
+				if (instance is null)
+					throw new InvalidOperationException("Python class is not found!");
+
+				return new ChatGPT(instance);
+			});
+
+			return services;
+		}
+	}
+}
