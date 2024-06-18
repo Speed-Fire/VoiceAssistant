068900b8557321fdd8313c07764ffb699059fe84
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-06-18 14:08:09 +0200

    Adding new models.

---
+++ VoiceAssistant.Domain/Models/AssistantScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Domain.Models
+{
+	public class AssistantScript
+	{
+	}
+}
