1603d209cb6d2f7c4259dd97011fd689cd2bedc1
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-07-11 14:11:31 +0200

    Removing command resolving settings.

--- VoiceAssistant/ViewModels/Settings/VoiceRecognitionSettingsVM.cs
+++ VoiceAssistant/ViewModels/Settings/VoiceRecognitionSettingsVM.cs
@@ -14,12 +14,10 @@ namespace VoiceAssistant.ViewModels.Settings
 {
 	internal partial class VoiceRecognitionSettingsVM(
 		SpeechToTextSettingsVM speech2TextSettings,
-		CommandResolvingSettingsVM commandResolvingSettings,
 		GeminiSettingsVM geminiSettings)
 		: ViewModel<VoiceRecognitionSettingsView>
 	{
 		public SpeechToTextSettingsVM SpeechToTextSettings { get; } = speech2TextSettings;
-		public CommandResolvingSettingsVM CommandResolvingSettings { get; } = commandResolvingSettings;
 		public GeminiSettingsVM GeminiSettings { get; } = geminiSettings;
 	}
 }
