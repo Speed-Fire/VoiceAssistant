a1f15b6ae4fd9b65012dbbe1e24644faf57a11f4
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-07-14 16:28:46 +0200

    Adding Optional.

---
+++ VoiceAssistant.Common/Optional.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+
+namespace VoiceAssistant.Common
+{
+	public class Optional<T>
+	{
+		public bool IsDefault { get; }
+		public T Value { get; }
+
+        public Optional()
+        {
+            IsDefault = true;
+        }
+
+        public Optional(T value)
+        {
+            Value = value;
+            IsDefault = false;
+        }
+
+        public static implicit operator Optional<T>(T value) => new(value);
+    }
+}
