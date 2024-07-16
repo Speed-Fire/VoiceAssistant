c78686959eea6ea52ba62d1589d9bd858cf824e2
Vladislav Sidorovich <vlad44812@gmail.com>
2024-07-16 14:16:28 +0200

    Adding UnderlyingCommand

---
+++ VoiceAssistant.Domain/Underlying/UnderlyingCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoiceAssistant.Domain.Models;
+
+#nullable disable
+
+namespace VoiceAssistant.Domain.Underlying
+{
+	public class UnderlyingCommand(long id)
+	{
+		public long Id { get; } = id;
+		public string Command { get; set; }
+		public string Input { get; set; }
+		public bool IsEnabledUser { get; set; }
+		public bool NeedsConfirmation { get; set; }
+
+		public UnderlyingScript Script { get; set; }
+
+		public bool IsEnabled => IsEnabledUser && Script is not null;
+
+		public static implicit operator UnderlyingCommand(AssistantAction command)
+			=> new(command.Id)
+			{
+				Command = command.Command,
+				Input = command.Input,
+				IsEnabledUser = command.IsEnabled,
+				NeedsConfirmation = command.NeedsConfirmation,
+			};
+	}
+}
