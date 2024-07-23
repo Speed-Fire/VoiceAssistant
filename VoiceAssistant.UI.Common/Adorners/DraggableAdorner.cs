231db2209e9f1bdae0ad84894f09c595591d845e
Vladislav Sidorovich <vlad44812@gmail.com>
2024-07-23 14:40:55 +0200

    Adding DraggableAdorner.

---
+++ VoiceAssistant.UI.Common/Adorners/DraggableAdorner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using VoiceAssistant.UI.Common.Helpers;
+
+namespace VoiceAssistant.UI.Common.Adorners
+{
+	public class DraggableAdorner : Adorner
+	{
+		private readonly FrameworkElement _element;
+		private readonly Brush _elementBrush;
+
+		private Point _mousePosition;
+
+		private Rect _elementRect;
+		private Point _location;
+		private Point _offset;
+
+		public DraggableAdorner(FrameworkElement adornedElement, Point offset) 
+			: base(adornedElement)
+		{
+			IsHitTestVisible = false;
+
+			_element = adornedElement;
+			_offset = offset;
+
+			_elementRect = new(adornedElement.RenderSize);
+			_elementBrush = new VisualBrush(adornedElement) { Opacity = 0.8 };
+		}
+
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			var p = _location;
+			p.Offset(-_offset.X, -_offset.Y);
+
+			_elementRect.Location = p;
+
+			drawingContext.DrawRectangle(_elementBrush, null, _elementRect);	
+		}
+
+		public void UpdatePosition(Point location)
+		{
+			this._location = location;
+			this.InvalidateVisual();
+		}
+
+		public void MoveToMousePosition()
+		{
+			MouseHelper.GetMousePos(ref _mousePosition);
+
+			var npos = _element.PointFromScreen(_mousePosition);
+
+			UpdatePosition(npos);
+		}
+	}
+}
---
+++ VoiceAssistant.UI.Common/Helpers/MouseHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VoiceAssistant.UI.Common.Helpers
+{
+	public static partial class MouseHelper
+	{
+		[LibraryImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static partial bool GetCursorPos(ref Win32Point p);
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct Win32Point
+		{
+			public Int32 X;
+			public Int32 Y;
+		}
+
+		public static Point GetMousePos()
+		{
+			Win32Point w32Mouse = new();
+			GetCursorPos(ref w32Mouse);
+
+			return new Point(w32Mouse.X, w32Mouse.Y);
+		}
+
+		public static void GetMousePos(ref Point point)
+		{
+			Win32Point w32Mouse = new();
+			GetCursorPos(ref w32Mouse);
+
+			point.X = w32Mouse.X;
+			point.Y = w32Mouse.Y;
+		}
+	}
+}
