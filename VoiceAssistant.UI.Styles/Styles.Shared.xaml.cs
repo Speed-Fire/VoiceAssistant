fab6aac9cc4a5c0f889f90fcee8271edae04f2a3
Vladislav Sidorovich <vlad44812@gmail.com>
2024-07-24 01:35:49 +0200

    Extracting UI styles to a different project.

---
+++ VoiceAssistant.UI.Styles/Styles.Shared.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.UI.Styles
+{
+	public partial class StylesSharedDictionary
+	{
+		private static readonly StylesSharedDictionary _instance = new();
+		public static StylesSharedDictionary Instance
+		{
+			get => _instance;
+		}
+
+        private StylesSharedDictionary()
+        {
+			InitializeComponent();
+        }
+    }
+}
---
+++ VoiceAssistant.UI.Styles/Styles.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace VoiceAssistant.UI.Styles
+{
+	public partial class StylesDictionary
+	{
+		private static readonly StylesDictionary _instance = new();
+		public static StylesDictionary Instance
+		{
+			get => _instance;
+		}
+
+		private StylesDictionary()
+		{
+			InitializeComponent();
+		}
+	}
+}
