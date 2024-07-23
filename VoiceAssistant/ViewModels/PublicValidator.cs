515677e31f21dd133258d149463340ba89e137c1
Vladislav Sidorovich <vlad44812@gmail.com>
2024-07-23 15:04:17 +0200

    Moving to view models.

---
+++ VoiceAssistant/ViewModels/PublicValidator.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VoiceAssistant.ViewModels
+{
+    public abstract class PublicValidator : ObservableValidator
+    {
+        public void ValidateAll()
+        {
+            ValidateAllProperties();
+        }
+
+        protected static T GetResource<T>(object key)
+        {
+            return (T)Application.Current.FindResource(key);
+        }
+
+        protected static T? TryGetResource<T>(object key)
+        {
+            return (T?)Application.Current.TryFindResource(key);
+        }
+    }
+}
